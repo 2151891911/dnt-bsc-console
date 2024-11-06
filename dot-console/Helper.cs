67d860fd0daeabe288814cf365fda5c550798bfd
kostas <boliskwstas@gmail.com>
2024-11-06 12:57:18 +0200

    updated feature

--- dot-console/Helper.cs
+++ dot-console/Helper.cs
@@ -8,7 +8,13 @@ class Helper
     //git: changed this
     public static int TestMath(int a, int b)
     {
-        return a + b;   
+        return a + b;
+    }
+
+    //git: change some aspect on feature
+    public static int Subtraction(int a, int b)
+    {
+        return a - b;
     }
 
 }
